a646ab87d49ffae5bd87536faa86ae4ccc935855
Sandro-Alessio Gierens <sandro@gierens.de>
2022-04-29 15:21:45 +0200

    Add class Server to execute commands on testsystem

--- autotest.py
+++ autotest.py
@@ -8,6 +8,9 @@ from argcomplete import autocomplete
 from configparser import ConfigParser
 from logging import (error, info, warn, debug, basicConfig,
                      DEBUG, INFO, WARN, ERROR)
+from dataclasses import dataclass
+from socket import getfqdn
+from subprocess import check_output
 
 
 # constants
@@ -17,6 +20,127 @@ LOG_LEVELS = {
     2: INFO,
     3: DEBUG,
 }
+
+
+# classes
+@dataclass
+class Server(object):
+    """
+    Server class.
+
+    This class represents a server.
+
+    Parameters
+    ----------
+
+    See Also
+    --------
+
+    Examples
+    --------
+    >>> Server('server.test.de')
+    Server(fqdn='server.test.de')
+    """
+    fqdn: str
+    localhost: bool = False
+    ssh_port: int = 22
+
+    def __post_init__(self):
+        """
+        Post initialization.
+
+        This method is called after the object is created.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        See Also
+        --------
+        __init__ : Initialize the object.
+        """
+        self.localhost = self.fqdn == 'localhost' or self.fqdn == getfqdn()
+
+    def __exec_local(self, command):
+        """
+        Execute a command on the localhost.
+
+        This method is called by the exec method if the server is localhost.
+
+        Parameters
+        ----------
+        command : str
+            The command to execute.
+
+        Returns
+        -------
+        str
+            The output of the command.
+
+        See Also
+        --------
+        exec : Execute command on the server.
+        __exec_ssh : Execute a command on the server over SSH.
+        """
+        return check_output(command, shell=True).decode('utf-8')
+
+    def __exec_ssh(self, command):
+        """
+        Execute a command on the server over SSH.
+
+        This method is called by the exec method if the server is not
+        localhost.
+
+        Parameters
+        ----------
+        command : str
+            The command to execute.
+
+        Returns
+        -------
+        str
+            The output of the command.
+
+        See Also
+        --------
+        exec : Execute command on the server.
+        __exec_local : Execute a command on the localhost.
+        """
+        return check_output(f'ssh -p {self.ssh_port} {self.fqdn} {command}',
+                            shell=True).decode('utf-8')
+
+    def exec(self, command):
+        """
+        Execute command on the server.
+
+        If the server is not localhost the command is executed over SSH.
+
+        Parameters
+        ----------
+        command : str
+            The command to execute.
+
+        Returns
+        -------
+        str
+            The output of the command.
+
+        See Also
+        --------
+        __exec_local : Execute a command on the localhost.
+        __exec_ssh : Execute a command on the server over SSH.
+
+        Example
+        -------
+        >>> print(server.exec('ls -l'))
+        .bashrc
+        """
+        if self.localhost:
+            return self.__exec_local(command)
+        else:
+            return self.__exec_ssh(command)
 
 
 # functions
@@ -164,10 +288,23 @@ def main():
     -------
     >>> main()
     """
+    # parse arguments, config file and setup logging
     parser = setup_parser()
     args = parse_args(parser)
     setup_logging(args)
     conf = setup_and_parse_config(args)
+
+    # create server objects
+    host = Server(conf['host']['fqdn'])
+    debug(f'host: {host}')
+    guest = Server(conf['guest']['fqdn'])
+    debug(f'guest: {guest}')
+    loadgen = Server(conf['loadgen']['fqdn'])
+    debug(f'loadgen: {loadgen}')
+
+    # test we can execute commands
+    print(host.exec('hostname -f'))
+    print(loadgen.exec('hostname -f'))
 
 
 if __name__ == '__main__':
