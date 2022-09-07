124ca8fcfc75e0d53e411fd73da1bd1833f810db
Sandro-Alessio Gierens <sandro@gierens.de>
2022-09-07 19:52:24 +0200

    Revise l2-load-latency.lua and add -r/--rate and -s/--size argument

--- moonprogs/l2-load-latency.lua
+++ moonprogs/l2-load-latency.lua
@@ -6,8 +6,6 @@ local stats  = require "stats"
 local hist   = require "histogram"
 local timer  = require "timer"
 
-local PKT_SIZE	= 60
-
 local function getRstFile(...)
 	local args = { ... }
 	for i, v in ipairs(args) do
@@ -23,7 +21,8 @@ function configure(parser)
 	parser:description("Generates bidirectional CBR traffic with hardware rate control and measure latencies.")
 	parser:argument("dev", "Device to transmit/receive from."):convert(tonumber)
 	parser:argument("mac", "MAC address of the destination device.")
-	parser:option("-r --rate", "Transmit rate in Mbit/s."):default(10000):convert(tonumber)
+	parser:option("-r --rate", "Transmit rate in kpps."):default(14000):convert(tonumber)
+    parser:option("-s --size", "Packet size in bytes."):default(60):convert(tonumber)
 	parser:option("-f --file", "Filename of the latency histogram."):default("histogram.csv")
 	parser:option("-t --time", "Time to transmit for in seconds."):default(60):convert(tonumber)
 end
@@ -31,8 +30,8 @@ end
 function master(args)
 	local dev = device.config({port = args.dev, rxQueues = 2, txQueues = 2})
 	device.waitForLinks()
-	dev:getTxQueue(0):setRate(args.rate)
-	mg.startTask("loadSlave", dev:getTxQueue(0), dev:getMac(true), args.mac)
+	dev:getTxQueue(0):setRate(args.rate * (args.size + 4) * 8 / 1000)
+	mg.startTask("loadSlave", dev:getTxQueue(0), dev:getMac(true), args.mac, args.size)
 	stats.startStatsTask{dev}
 	mg.startSharedTask("timerSlave", dev:getTxQueue(1), dev:getRxQueue(1), args.mac, args.file)
 	if args.time >= 0 then
@@ -43,7 +42,7 @@ function master(args)
 	mg.waitForTasks()
 end
 
-function loadSlave(queue, srcMac, dstMac)
+function loadSlave(queue, srcMac, dstMac, pktSize)
 	local mem = memory.createMemPool(function(buf)
 		buf:getEthernetPacket():fill{
 			ethSrc = srcMac,
@@ -53,7 +52,7 @@ function loadSlave(queue, srcMac, dstMac)
 	end)
 	local bufs = mem:bufArray()
 	while mg.running() do
-		bufs:alloc(PKT_SIZE)
+		bufs:alloc(pktSize)
 		queue:send(bufs)
 	end
 end
@@ -68,4 +67,3 @@ function timerSlave(txQueue, rxQueue, dstMac, histfile)
 	hist:print()
 	hist:save(histfile)
 end
-
