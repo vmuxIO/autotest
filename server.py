76a871b152f1a403d7e2046e2ed01504f4114ae5
Sandro-Alessio Gierens <sandro@gierens.de>
2022-11-20 19:17:29 +0100

    Comment out cdrom stuff in Host.run_guest
    We don't use cloud init but preconfigured NixOS images.

--- server.py
+++ server.py
@@ -1239,7 +1239,7 @@ class Host(Server):
             f' -device virtio-blk-{dev_type},id=rootdisk,drive=root' +
             (',use-ioregionfd=true' if ioregionfd else '')
             + f',queue-size={rx_queue_size}'
-            ' -cdrom /home/networkadmin/images/guest_init.iso' +
+            # ' -cdrom /home/networkadmin/images/guest_init.iso' +
             ' -serial stdio' +
             ' -monitor tcp:127.0.0.1:2345,server,nowait' +
             ' -netdev tap,vhost=on,id=admin0,ifname=tap0,script=no,' +
